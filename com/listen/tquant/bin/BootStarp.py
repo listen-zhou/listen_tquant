d26566d3d567b649fd66f7bfee64826aaca59fed
listen-zhou <769133787@qq.com>
2017-03-18 17:37:19 +0800

    打开注释行，并增加中文注释

--- com/listen/tquant/bin/BootStarp.py
+++ com/listen/tquant/bin/BootStarp.py
@@ -6,30 +6,33 @@ from com.listen.tquant.service.stock.StockDayKlineService import StockDayKlineSe
 from com.listen.tquant.service.stock.CalendarService import CalendarService
 from com.listen.tquant.service.stock.StockDayKlineIncrementService import StockDayKlineIncrementService
 
-from apscheduler.schedulers.blocking import BlockingScheduler
 from com.listen.tquant.dbservice.Service import DbService
 
 
 threads = []
 
-# dbService1 = DbService()
-# stockInfoService = StockInfoService(dbService1)
-# stockInfoThread = threading.Thread(target=stockInfoService.get_stock_info)
-# stockInfoThread.setName('stockInfoThread')
-# threads.append(stockInfoThread)
+# 处理股票基本信息线程
+dbService1 = DbService()
+stockInfoService = StockInfoService(dbService1)
+stockInfoThread = threading.Thread(target=stockInfoService.get_stock_info)
+stockInfoThread.setName('stockInfoThread')
+threads.append(stockInfoThread)
 #
-# dbService2 = DbService()
-# stockDayKlineService = StockDayKlineService(dbService2)
-# stockDayKlineThread = threading.Thread(target=stockDayKlineService.get_all_stock_day_kline)
-# stockDayKlineThread.setName('stockDayKlineThread')
-# threads.append(stockDayKlineThread)
+# 处理股票日K数据线程
+dbService2 = DbService()
+stockDayKlineService = StockDayKlineService(dbService2)
+stockDayKlineThread = threading.Thread(target=stockDayKlineService.get_all_stock_day_kline)
+stockDayKlineThread.setName('stockDayKlineThread')
+threads.append(stockDayKlineThread)
 #
-# dbService3 = DbService()
-# calendarService = CalendarService(dbService3)
-# calendarServiceThread = threading.Thread(target=calendarService.get_calendar_info)
-# calendarServiceThread.setName('calendarServiceThread')
-# threads.append(calendarServiceThread)
+# 处理证券交易日信息线程
+dbService3 = DbService()
+calendarService = CalendarService(dbService3)
+calendarServiceThread = threading.Thread(target=calendarService.get_calendar_info)
+calendarServiceThread.setName('calendarServiceThread')
+threads.append(calendarServiceThread)
 
+# 处理增量股票日K数据线程，即已经处理过的股票的近几日的日K数据
 dbService4 = DbService()
 stockDayKlineIncrementService = StockDayKlineIncrementService(dbService4)
 stockDayKlineIncrementThread = threading.Thread(target=stockDayKlineIncrementService.get_all_stock_day_kline)
