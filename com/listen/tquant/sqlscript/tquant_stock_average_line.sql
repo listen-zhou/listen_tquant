03bc8305cef88ce28d7ea716a41d2237a25cb1a2
listen <769133787@qq.com>
2017-04-12 01:21:25 +0800

    修改数据库

--- com/listen/tquant/sqlscript/tquant_stock_average_line.sql
+++ com/listen/tquant/sqlscript/tquant_stock_average_line.sql
@@ -12,21 +12,22 @@ CREATE TABLE `tquant_stock_average_line` (
 	`amount` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '当日成交额(元)',
 	`amount_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交额(元)',
 	`amount_pre_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一ma日均成交额(元)',
-	`amount_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均交易额变动幅度（涨跌幅）',
-	`amount_avg_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均交易额变动幅度（涨跌幅）均',
+	`amount_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均交易额涨跌幅',
+	`amount_avg_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均交易额涨跌幅均',
 	`vol` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '当日成交量(手)',
 	`vol_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交量(手)',
 	`vol_pre_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一ma日均成交量(手)',
-	`vol_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交量变动幅度（涨跌幅）',
-	`vol_avg_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交量变动幅度（涨跌幅）均',
+	`vol_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交量涨跌幅',
+	`vol_avg_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交量涨跌幅均',
 	`price_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交价价（平均成交价）',
 	`price_pre_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一ma日均成交价',
-	`price_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交价变动幅度（涨跌幅）',
-	`price_avg_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交价变动幅度（涨跌幅）均',
+	`price_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交价涨跌幅',
+	`price_avg_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT 'ma日均成交价涨跌幅均',
 	`amount_flow_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '日金钱流向涨跌幅',
 	`amount_flow_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '日金钱流向涨跌幅均',
 	`vol_flow_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '日成交量流向涨跌幅',
 	`vol_flow_chg_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '日成交量流向涨跌幅均',
+	`close_ma_price_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '收盘价/ma日均价幅度',
 	`auto_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '时间戳',
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `security_code_the_date_ma` (`security_code`, `the_date`, `ma`, `exchange_code`),
@@ -37,4 +38,5 @@ CREATE TABLE `tquant_stock_average_line` (
 COMMENT='股票均线数据\r\nma日均收盘价=sum(前ma日(含)的收盘价)/ma\r\nma日均收盘价涨跌幅=(ma日均收盘价 - 前一ma日均收盘价)/前一ma日均收盘价 * 100\r\nma日均收盘价涨跌幅均=sum(前ma日(含)均收盘价涨跌幅)/ma\r\n\r\nma日均成交额=sum(前ma日(含)的成交额)/ma\r\nma日均成交额涨跌幅=(ma日均成交额 - 前一ma日均成交额)/前一ma日均成交额 * 100\r\nma日均成交额涨跌幅均=sum(前ma日(含)均成交额涨跌幅)/ma\r\n\r\nma日均成交量=sum(前ma日(含)的成交量)/ma\r\nma日均成交量涨跌幅=(ma日均成交量 - 前一ma日均成交量)/前一ma日均成交量 * 100\r\nma日均成交量涨跌幅均=sum(前ma日(含)均成交量涨跌幅)/ma\r\n\r\nma日均成交价=sum(前ma日(含)的成交额)/sum(ma日(含)的成交量)\r\nma日均成交价涨跌幅=(ma日均成交价 - 前一ma日均成交价)/前一ma日均成交价 * 100\r\nma日均成交价涨跌幅均=sum(前ma日(含)均成交价涨跌幅)/ma\r\n\r\n日金钱流向涨跌幅=日成交额/ma日(含)均成交额 * 100\r\n日金钱流向涨跌幅均=sum(前ma日(含)金钱流向涨跌幅)/ma\r\n\r\n日成交量流向涨跌幅=日成交量/ma日(含)均成交量 * 100\r\n日成交量流向涨跌幅均=sum(前ma日(含)成交量流向涨跌幅)/ma\r\n'
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
+AUTO_INCREMENT=15644
 ;
--- com/listen/tquant/sqlscript/tquant_stock_day_kline.sql
+++ com/listen/tquant/sqlscript/tquant_stock_day_kline.sql
@@ -4,17 +4,20 @@ CREATE TABLE `tquant_stock_day_kline` (
 	`the_date` DATE NOT NULL COMMENT '交易日',
 	`exchange_code` VARCHAR(20) NOT NULL COMMENT '交易所标识,SZ,SH',
 	`amount` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '交易额(元)',
-	`previous_amount` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一日交易额(元)',
-	`amount_change_percent` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '交易额波动幅度（涨跌幅）',
+	`amount_pre` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一日交易额(元)',
+	`amount_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '交易额涨跌幅',
 	`vol` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '交易量(手)',
-	`previous_vol` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一日交易量(手)',
-	`vol_change_percent` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '交易量波动幅度（涨跌幅）',
+	`vol_pre` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一日交易量(手)',
+	`vol_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '交易量涨跌幅',
 	`open` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '开盘价',
 	`high` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '最高价',
 	`low` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '最低价',
 	`close` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '收盘价',
-	`previous_close` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一日收盘价',
-	`close_change_percent` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '收盘价波动幅度（涨跌幅）',
+	`close_pre` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '前一日收盘价',
+	`close_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '收盘价涨跌幅',
+	`price_avg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '日均价',
+	`close_price_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '收盘/日均价百分比',
+	`price_avg_chg` DECIMAL(20,2) NULL DEFAULT NULL COMMENT '日均价涨跌幅百分比',
 	`auto_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '时间戳',
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `security_code_the_date` (`security_code`, `the_date`, `exchange_code`),
@@ -23,4 +26,5 @@ CREATE TABLE `tquant_stock_day_kline` (
 COMMENT='股票日K数据'
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
+AUTO_INCREMENT=2944
 ;
